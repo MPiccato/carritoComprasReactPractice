d67e78d11d95a095fe525baf1363dc58673aaa25
MP Data - Trading <42841276+MPiccato@users.noreply.github.com>
2024-10-12 19:26:10 -0300

    hecho el useReducer del CartProvider

--- src/context/CartProvider.jsx
+++ src/context/CartProvider.jsx
@@ -1,11 +1,69 @@
-import { CardContent } from "@mui/material"
-import { CardContext } from "./CardContext"
+import { useReducer } from "react";
+import { CardContext } from "./CardContext";
+
+export const CartProvider = ({ children }) => {
+  const initialState = [];
+
+  const [shoppingList, dispatch] = useReducer(shoppingReducer, initialState);
+
+  const addProduct = (product) => {
+    const action = {
+      type: "[Cart] Agregar Producto",
+      payload: product,
+    };
+    dispatch(action);
+  };
+
+  const removeProduct = (id) => {
+    const action = {
+      type: "[Cart] Quitar Producto",
+      payload: id,
+    };
+    dispatch(action);
+  };
+
+  const incrementarProducto = (id) => {
+    const action = {
+      type: "[Cart] Incrementar Producto",
+      payload: id,
+    };
+    dispatch(action);
+  };
+
+  const decrementarProducto = (id) => {
+    const action = {
+      type: "[Cart] Decrementar Producto",
+      payload: id,
+    };
+    dispatch(action);
+  };
+
+  const shoppingReducer = (state = initialState, action ={}) => {
+    switch (action.type) {
+      case "[Cart] Agregar Producto" :
+        
+        break;
+      case "[Cart] Quitar Producto":
+        break;
+      
+      case "[Cart] Incrementar Producto":
+        break;
+      
+      case "[Cart] Decrementar Producto":
+        break
 
 
-export const CartProvider = ({children}) => {
+      
+      default:
+        return state
+        
+    }
+  }
+
+
+
+
   return (
-    <CardContext.Provider>
-        {children}
-    </CardContext.Provider>
-  )
-}
+    <CardContext.Provider value={{ products }}>{children}</CardContext.Provider>
+  );
+};
