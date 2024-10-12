925b2d8ee80099b58253249d0b61dd2866bf968c
MP Data - Trading <42841276+MPiccato@users.noreply.github.com>
2024-10-12 12:44:25 -0300

    se agregaron mui y sweetalert2. Se configura NavLink

--- src/components/CarritoApp.jsx
+++ src/components/CarritoApp.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+
+import { Navigate, Routes, Route } from 'react-router-dom'
+import { NavBar } from './NavBar'
+import { ProductsPage } from '../pages/ProductsPage'
+import { CartPage } from '../pages/CartPage'
 
 export const CarritoApp = () => {
   return (
-    <div>CarritoApp</div>
+    <>
+        <NavBar/>
+       
+       <div>
+        <Routes>
+            <Route path='/' element={<ProductsPage/>}></Route>
+            <Route path='/carrito' element={<CartPage/>}></Route>
+            <Route path='/*' element={<Navigate to='/'/>}></Route>
+        </Routes>
+       </div>
+    </>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,40 @@
+import { Badge } from "@mui/material";
+
+import { ShoppingCart } from "@mui/icons-material";
+import { NavLink } from "react-router-dom";
+export const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <NavLink to="/" className="navbar-brand" href="#">
+          Carrito de Compras
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink to="/" className="nav-link active" aria-current="page">
+                Productos
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <NavLink to='/carrito'>
+          <Badge badgeContent={4} color="primary">
+            <ShoppingCart />
+          </Badge>
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
